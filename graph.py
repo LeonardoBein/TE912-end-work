ff82c7c62e55ca885741eaf174b120a7f5d366e1
Vinicius Parede <vinicius.parede@ufpr.br>
2022-04-10 16:11:55 -0300

    fix directory call

--- graph.py
+++ graph.py
@@ -1,15 +1,71 @@
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
-#from tensorflow.keras.models import load_weights
+import os
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+
+num_classes = 10
+_, (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_test = x_test.astype("float32") / 255
+x_test = np.expand_dims(x_test, -1)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
 def accuracy_model (model_number):
-    path = '/home/leonardo/Documentos/UFPR/ia/te941-work-2/model_train'
+    path = os.getcwd() + '/model_train'
     model_name = '/mnist_model_'
-    weights_name = '/mnist_model_weights_'
+    csv_name = '/mnist_model_dataframe_'
     extension = '.h5'
 
     model = load_model(path + model_name + str(model_number) + extension)
-    model.load_weights('/home/leonardo/Documentos/UFPR/ia/te941-work-2/model_train/mnist_model_weights_1.h5')
+    score = model.evaluate(x_test, y_test, verbose=0)
+    df = pd.read_csv(path + csv_name + str(model_number) + '.csv')
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+
+    epochs = range(len(df['accuracy']))
+    plt.plot(epochs, df['accuracy'], 'bo', label='Training accuracy')
+    plt.plot(epochs, df['val_accuracy'], 'red', label='Validation accuracy')
+    plt.title('Model' + ': {}'.format(model_number) + ' - Accuracy: {:.3f}'.format(score[1]))
+    plt.legend()
+    plt.show()
+
+    # some correct number images
+    predicted_classes = model.predict(x_test)
+    predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+    test_y = np.argmax(y_test, axis=1)
+    correct = np.where(predicted_classes == test_y)[0]
     
+    print("Found %d correct labels" % len(correct))
+    for i, correct in enumerate(correct[:9]):
+        plt.subplot(3,3,i+1)
+        plt.imshow(x_test[correct].reshape(28,28), cmap='gray', interpolation='none')
+        plt.title("Predicted {}, Correct {}".format(predicted_classes[correct], test_y[correct]))
+        plt.tight_layout()
+    plt.show()
+
+    # some incorrect numbers images
+    predicted_classes = model.predict(x_test)
+    predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+    test_y = np.argmax(y_test, axis=1)
+    correct = np.where(predicted_classes != test_y)[0]
+    
+    print("Found %d incorrect labels" % len(correct))
+    for i, correct in enumerate(correct[:9]):
+        plt.subplot(3,3,i+1)
+        plt.imshow(x_test[correct].reshape(28,28), cmap='gray', interpolation='none')
+        plt.title("Predicted {}, Correct {}".format(predicted_classes[correct], test_y[correct]))
+        plt.tight_layout()
+    plt.show()
+
 def compare_models():
-    pass
+    path_to_dir = os.getcwd() + '/model_train'
+    suffix = '.h5'
+    filenames = os.listdir(path_to_dir)
+    list_models_name = [ filename for filename in filenames if filename.endswith( suffix ) ]
+
+    list_models = list()
+    for list_model_name in list_models_name:
+        #path = os.getcwd() + 
+        model = load_model(os.get)
+        list_models.append(model)
+
--- model_train/model_train.py
+++ model_train/model_train.py
@@ -1,8 +1,28 @@
 import numpy as np
 from tensorflow import keras
 from models import CNNModelAbstract
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import os
+import pandas as pd
+
+
+def create_dataframe (model_train) -> pd.DataFrame:
+    accuracy = model_train.history['accuracy']
+    val_accuracy = model_train.history['val_accuracy']
+    loss = model_train.history['loss']
+    val_loss = model_train.history['val_loss']
+
+    data = {
+        'accuracy': accuracy,
+        'val_accuracy': val_accuracy,
+        'loss': loss,
+        'val_loss': val_loss
+    }
+
+    df = pd.DataFrame(data)
+
+    return df
+
 
 def train_model(model_template: CNNModelAbstract, model_number: int):
     
@@ -10,6 +30,7 @@ def train_model(model_template: CNNModelAbstract, model_number: int):
     num_classes = model_template.num_classes
 
     # the data, split between train and test sets
+    # 60000 images for train and 10000 images for test
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
     # Scale images to the [0, 1] range
@@ -23,24 +44,25 @@ def train_model(model_template: CNNModelAbstract, model_number: int):
     # convert class vectors to binary class matrices
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
-
-    # split data: 80% (48000) for train model and 20% (12000) for valid model
-    train_X,valid_X,train_label,valid_label = train_test_split(x_train, y_train, test_size=0.2, random_state=13)
     
     # train the model
     batch_size = 256
     epochs = 20
-
     model_template.model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-    model_train = model_template.model.fit(train_X, train_label, batch_size=batch_size, epochs=epochs, validation_data=(valid_X, valid_label))
+    print('Training model ' + str(model_number) + '...')
+    model_train = model_template.model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+    df = create_dataframe(model_train)
+
+    print('Model ' + str(model_number) + ' trained!')
 
     # save the model
-    path = '/home/leonardo/Documentos/UFPR/ia/te941-work-2/model_train'
+    path = os.getcwd() + '/model_train'
     try:
         model_template.model.save(path + '/mnist_model_' + str(model_number) + '.h5')
         print('Model ' + str(model_number) + ' Saved!')
-        model_train.model.save_weights(path + '/mnist_model_weights_' + str(model_number) + '.h5')
-        print('Model Weights ' + str(model_number) + ' Saved!')
+        df.to_csv(path + '/mnist_model_dataframe_' + str(model_number) + '.csv')
+        print('Model Dataframe' + str(model_number) + ' Saved!')
         
     except Exception as error:
         print(error)
